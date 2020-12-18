bebf2d2c942b9a5365fc8d5d5725db099473d247
Piotr Rześny <rzesnypPiotrr@gmail.com>
2020-12-18 05:51:11 +0100

    lets go firebase

---
+++ src/app/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDEpODOTx_6mIjDDgZ5KZnbpoea8QzeJdQ',
+	authDomain: 'linkedin-clone-479d7.firebaseapp.com',
+	projectId: 'linkedin-clone-479d7',
+	storageBucket: 'linkedin-clone-479d7.appspot.com',
+	messagingSenderId: '255169642709',
+	appId: '1:255169642709:web:5f80831200d58076f7a25c',
+	measurementId: 'G-182DXV0YL1',
+};
